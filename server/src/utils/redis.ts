146dedc17de164523ed12c75ea2b7b46a2c84a54
VishalFulkar <vishalfulkar9575@gmail.com>
2026-07-30 21:59:26 +0530

    feat: initialize server project with Redis and PostgreSQL database connections

---
+++ server/src/utils/redis.ts
@@ -0,0 +1,21 @@
+import Redis from 'redis';
+
+const redis = Redis.createClient({
+    url: process.env.REDIS_URL || "redis://localhost:6379"
+});
+
+redis.on("error", (err) => {
+    console.log("Redis error", err);
+});
+
+export async function connectRedis() {
+    try {
+        await redis.connect();
+        console.log("Redis connected");
+    }
+    catch (err: any) {
+        console.log("Redis connection error: ", err);
+    }
+}
+
+export default redis;
